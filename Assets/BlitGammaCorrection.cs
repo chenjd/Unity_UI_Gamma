3e11951dc8687e5c498ca79b2794443a7bb90fe1
chenjd <chen.jiadong@outlook.com>
2019-10-31 08:27:17 +0800

    enable/disable gamma correction cam depends on color space/

--- Assets/BlitGammaCorrection.cs
+++ Assets/BlitGammaCorrection.cs
@@ -2,13 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class BlitGammaCorrection : MonoBehaviour {
 
     
     public Shader m_BlitShader;
+    public Camera m_Cam;
     private Material m_BlitMat;
+
+    void Update()
+    {
+        if(m_Cam == null)
+        {
+            m_Cam = GetComponent<Camera>();
+        }
+        m_Cam.enabled = PlayerSettings.colorSpace == ColorSpace.Linear;
+    }
     
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
